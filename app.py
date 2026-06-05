de26bd9baccfd42f673d3b126dfea0ad1d6cbe8e
burhankaya289-cmyk <burhankaya289@gmail.com>
2026-06-05 21:53:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,4 +8,13 @@ st.set_page_config(
 
 st.title("PTT Kargo Panel")
 
-st.success("Kurulum başarılı")
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.metric("Toplam Gönderi", "0")
+
+with col2:
+    st.metric("Aylık Gönderi", "0")
+
+with col3:
+    st.metric("Kalan Barkod", "0")
