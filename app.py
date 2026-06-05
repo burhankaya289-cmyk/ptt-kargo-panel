56ef5699258feb85f8075da27f3199425260856d
burhankaya289-cmyk <burhankaya289@gmail.com>
2026-06-05 21:58:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,13 +8,4 @@ st.set_page_config(
 
 st.title("PTT Kargo Panel")
 
-col1, col2, col3 = st.columns(3)
-
-with col1:
-    st.metric("Toplam Gönderi", "0")
-
-with col2:
-    st.metric("Aylık Gönderi", "0")
-
-with col3:
-    st.metric("Kalan Barkod", "0")
+st.info("Sol menüden sayfa seçiniz.")
